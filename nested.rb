6a18e03d4c6c71b5d3e61601a9a8c311eb4232a1
William Pescherine <wkpescherine@gmail.com>
2019-05-28 04:59:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,6 +83,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+  programmer_hash.merge(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ })
 
     
 end

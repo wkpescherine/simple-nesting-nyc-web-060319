b314f9b0cebff5cb6a8d9a17a74807a47afd3f96
William Pescherine <wkpescherine@gmail.com>
2019-05-28 05:01:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,5 +132,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
+
 
 end
